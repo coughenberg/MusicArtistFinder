0249e6fc4b5fc8746096c1ec0b7659ec0536bebe
Tim Kaufenberg <khfantk@gmail.com>
2021-01-19 17:03:39 -0600

    updating code js -> ts, npm run dev succeeds on port 4000

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./src/index');
+var usersRouter = require('./src/users');
 
 var app = express();
 
